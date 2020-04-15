b68f32d34c9252e005569d849b17c6af678991d1
josgirrui <dragojola@gmail.com>
2020-04-15 13:07:50 +0200

    Generics

--- server/controllers/generic.controller.ts
+++ server/controllers/generic.controller.ts
@@ -1,70 +1,69 @@
 import { Request, Response, NextFunction } from 'express';
 import Logger from '../loaders/logger'
-import {IGenericInterface} from '../interfaces/IGenericInterface'
+import { IGenericInterface } from '../interfaces/IGenericInterface'
 import { IUsuarioDTO } from '../interfaces/IUsuario';
 import GenericService from '../services/generic.services';
 
-export default class GenericController{
+export default class GenericController {
 
-    private myService:GenericService;
+    private myService: GenericService;
     private serviceString: string;
 
-    constructor(clase:GenericService){
+    constructor(clase: GenericService) {
         this.myService = clase;
         this.serviceString = this.myService.name();
     }
     public create = async (req: Request, res: Response, next: NextFunction) => {
-        Logger.debug('Creando: '+this.serviceString+".");
+        Logger.debug('Creando: ' + this.serviceString + ".");
         try {
             const myObject = await this.myService.create(req.body as IGenericInterface, req.user as IUsuarioDTO);
-            var DTO = { status: 200,};
             return res.status(200).json({ status: 200, [this.serviceString]: myObject });
         } catch (e) {
-            Logger.error('Se ha producido un error creando : '+this.serviceString+".");
+            Logger.error('Se ha producido un error creando : ' + this.serviceString + ".");
             Logger.error(e);
             return res.status(400).json({ status: 400, message: "Se ha producido un error inesperado. Contacte con el administrador." });
         }
     }
     public edit = async (req: Request, res: Response, next: NextFunction) => {
-        Logger.debug('Editando : '+this.serviceString+".");
+        Logger.debug('Editando : ' + this.serviceString + ".");
         try {
-            const myObject = await this.myService.edit(req.body as IGenericInterface, req.user as IUsuarioDTO);
-            return res.status(200).json({ status: 200, [this.serviceString]: myObject });
+            await this.myService.edit(req.body as IGenericInterface, req.user as IUsuarioDTO);
+            return res.status(200).json({ status: 200 });
         } catch (e) {
-            Logger.error('Se ha producido un error editando : '+this.serviceString+".");
+            Logger.error('Se ha producido un error editando : ' + this.serviceString + ".");
             Logger.error(e);
             return res.status(400).json({ status: 400, message: "Se ha producido un error inesperado. Contacte con el administrador." });
         }
     }
     public delete = async (req: Request, res: Response, next: NextFunction) => {
-        Logger.debug('Borrando : '+this.serviceString+".");
+        Logger.debug('Borrando : ' + this.serviceString + ".");
         try {
-            const myObject = await this.myService.delete((req.body as IGenericInterface)._id);
+            await this.myService.delete((req.body as IGenericInterface)._id);
             return res.status(200).json({ status: 200, mensaje: "Borrado con exito" });
         } catch (e) {
-            Logger.error('Se ha producido un error borrando : '+this.serviceString+".");
+            Logger.error('Se ha producido un error borrando : ' + this.serviceString + ".");
             Logger.error(e);
             return res.status(400).json({ status: 400, message: "Se ha producido un error inesperado. Contacte con el administrador." });
         }
     }
     public findAll = async (req: Request, res: Response, next: NextFunction) => {
-        Logger.debug('Metodo findAll : '+this.serviceString+".");
+        Logger.debug('Metodo findAll : ' + this.serviceString + ".");
         try {
             const myObjects = await this.myService.findAll();
             return res.status(200).json({ status: 200, [this.serviceString]: myObjects });
         } catch (e) {
-            Logger.error('Se ha producido un error findAll : '+this.serviceString+".");
+            Logger.error('Se ha producido un error findAll : ' + this.serviceString + ".");
             Logger.error(e);
             return res.status(400).json({ status: 400, message: "Se ha producido un error inesperado. Contacte con el administrador." });
         }
     }
     public findById = async (req: Request, res: Response, next: NextFunction) => {
-        Logger.debug('Metodo findById : '+this.serviceString+".");
+        Logger.debug('Metodo findById : ' + this.serviceString + ".");
         try {
             const myObject = await this.myService.findById((req.body as IGenericInterface)._id);
             return res.status(200).json({ status: 200, [this.serviceString]: myObject });
         } catch (e) {
-            Logger.error('Se ha producido un error findById : '+this.serviceString+".");
+            Logger.error('Se ha producido un error findById : ' + this.serviceString + ".");
             Logger.error(e);
             return res.status(400).json({ status: 400, message: "Se ha producido un error inesperado. Contacte con el administrador." });
         }
--- server/interfaces/IGenericInterface.ts
+++ server/interfaces/IGenericInterface.ts
@@ -1,3 +1,5 @@
-export interface IGenericInterface{
+export interface IGenericInterface {
     _id: string;
+    _version: Number;
+    updated_for: string;
 }
