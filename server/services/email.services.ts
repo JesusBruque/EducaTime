2a6f4c7e01937255f2fd7304f10cff041b00edf1
Pepon Garcia <pepeg93@hotmail.com>
2020-04-28 10:18:31 +0200

    email de confirmacion de registro

--- server/services/email.services.ts
+++ server/services/email.services.ts
@@ -3,6 +3,8 @@ import nodemailer from 'nodemailer';
 
 const transport = {
     host: config.email_host,
+    port:587,
+    secure:true,
     auth: {
         user:config.email_user,
         pass: config.email_pass
@@ -16,15 +18,13 @@ transporter.verify((err,success) => {
 });
 
 
-export const sendEmail = (emailTo,subject,htmlMessage) => {
+export const sendEmail = async (emailTo,subject,htmlMessage) => {
     const email = {
         from: `Casor.Academia de formación deportiva <formacion@casor.com>`,
         to: emailTo,
         subject: subject,
         html: htmlMessage,
     };
-    transporter.sendEmail(email,(err,data) => {
-        if(err) console.error(err);
-        if(data) console.log(data);
-    });
+    await transporter.sendEmail(email);
 };
+
