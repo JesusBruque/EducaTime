2faa14b358cde2c60597ea1addc14f50ecf65190
Pepon Garcia <pepeg93@hotmail.com>
2020-05-16 17:38:21 +0200

    fix entrar en curso

--- components/whiteBoard/CursoOpen.tsx
+++ components/whiteBoard/CursoOpen.tsx
@@ -289,7 +289,7 @@ const CursoOpen: FunctionComponent<Props> = (props) => {
     return (
         <div>
             <h2>{curso.title}</h2>
-            <ValorarContent user={props.user} cursoIndex={props.cursoIndex} cursoId={curso}/>
+            {/*<ValorarContent user={props.user} cursoIndex={props.cursoIndex} cursoId={curso}/>*/}
             <Button action={handleShowValorarWindow} color = {'var(--main-color)'} text= 'Valorar este curso' disabled= {false} type = {'button'}/>
             {curso.lections.map((lection,i) => {
                 return(
